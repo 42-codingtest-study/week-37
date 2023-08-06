c715999c02dfd1402364f1263f09155691a64d67
Y2IN <dpdls0228@daum.net>
2023-08-06 23:04:36 +0900

    week37

---
+++ 9372.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, input().split())
+    
+    for j in range(m):
+        a, b = map(int, input().split())
+        
+    # 국가의 수 - 1
+    print(n - 1)
